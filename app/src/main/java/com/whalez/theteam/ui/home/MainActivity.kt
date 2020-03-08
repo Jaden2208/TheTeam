37eaa6e095011b50eaa0bda3ffebb7799342c559
ryu22 <ryu2208@naver.com>
2020-03-08 22:03:06 +0900

    - MainActivity에 FrameLayout, NavigationView, 홈, 내 팀, 사용자, 설정 fragment 적용
    --- To do ---
    
    - MainActivity UI 구상
    
    etc...

--- app/src/main/java/com/whalez/theteam/ui/home/MainActivity.kt
+++ app/src/main/java/com/whalez/theteam/ui/home/MainActivity.kt
@@ -2,12 +2,61 @@ package com.whalez.theteam.ui.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.auth.FirebaseAuth
 import com.whalez.theteam.R
+import com.whalez.theteam.ui.sign.ErrorFragment
+import com.whalez.theteam.ui.utils.finishAndGoToLoginActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val firebaseAuth = FirebaseAuth.getInstance()
+    private val user = firebaseAuth.currentUser
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        checkLoginStatus()
+
+//        btn_logout.setOnClickListener {
+//            firebaseAuth.signOut()
+//            finishAndGoToLoginActivity(applicationContext, this)
+//            Toast.makeText(this, "로그아웃되었습니다.", Toast.LENGTH_SHORT).show()
+//        }
+
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fl_main, HomeFragment()).commitAllowingStateLoss()
+
+        nv_main.setOnNavigationItemSelectedListener(ItemSelectedListener())
+    }
+
+    private fun checkLoginStatus() {
+        if(user != null){
+//            text_view.text = user.email
+        } else {
+//            text_view.text = "로그인 되어있지 않음."
+            finishAndGoToLoginActivity(applicationContext, this)
+        }
+    }
+
+    inner class ItemSelectedListener : BottomNavigationView.OnNavigationItemSelectedListener {
+        override fun onNavigationItemSelected(item: MenuItem): Boolean {
+            val transaction = supportFragmentManager.beginTransaction()
+            val fragment = when(item.itemId){
+                R.id.home_item -> HomeFragment()
+                R.id.team_item -> TeamFragment()
+                R.id.user_item -> UserFragment()
+                R.id.setting_item -> SettingFragment()
+                else -> ErrorFragment()
+            }
+            transaction.replace(R.id.fl_main, fragment).commitAllowingStateLoss()
+            return true
+        }
     }
 }
+
+
--- app/src/main/java/com/whalez/theteam/ui/utils/ConstMethods.kt
+++ app/src/main/java/com/whalez/theteam/ui/utils/ConstMethods.kt
@@ -1,7 +1,14 @@
 package com.whalez.theteam.ui.utils
 
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.content.Intent.FLAG_ACTIVITY_NEW_TASK
 import android.view.View
 import android.widget.FrameLayout
+import androidx.core.content.ContextCompat.startActivity
+import com.whalez.theteam.ui.home.MainActivity
+import com.whalez.theteam.ui.sign.LoginActivity
 
 fun showLoading(frameLayout: FrameLayout) {
     frameLayout.visibility = View.VISIBLE
@@ -9,4 +16,10 @@ fun showLoading(frameLayout: FrameLayout) {
 
 fun hideLoading(frameLayout: FrameLayout) {
     frameLayout.visibility = View.GONE
+}
+
+fun finishAndGoToLoginActivity(context: Context, activity: Activity){
+    val intent = Intent(context, LoginActivity::class.java)
+    context.startActivity(intent.addFlags(FLAG_ACTIVITY_NEW_TASK))
+    activity.finish()
 }
